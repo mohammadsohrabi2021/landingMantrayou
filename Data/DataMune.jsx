514ddbf9fde35934a945ef9887ccac1c2485b331
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-08-24 15:41:58 +0330

    create data file dataMuneHeader done

---
+++ Data/DataMune.jsx
@@ -0,0 +1,6 @@
+export const dataMuneHeader = [
+  { id: 1, name: "صفحه اصلی", href: "/" },
+  { id: 2, name: "بررسی ها", href: "/reviews" },
+  { id: 3, name: "تیم ما", href: "/about-us" },
+  { id: 4, name: "تماس باما", href: "/contact-us" },
+];
