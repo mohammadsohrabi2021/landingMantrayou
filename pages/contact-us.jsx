50442f1efd9df471f377e405af7f67964c1837f0
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-08-24 15:40:46 +0330

    create route component ContactUs done

---
+++ pages/contact-us.jsx
@@ -0,0 +1,10 @@
+import ContactUsPage from '@/components/pages/ContactUsPage'
+import React from 'react'
+
+function ContactUs() {
+  return (
+    <ContactUsPage/>
+  )
+}
+
+export default ContactUs
