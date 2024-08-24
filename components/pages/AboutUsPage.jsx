941d986f5fa026cfdb20edbd44ac0851e28dc8c3
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-08-24 15:31:06 +0330

    create component AboutUsPage to project done

---
+++ components/pages/AboutUsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MeetTheTeam from '../template/MeetTheTeam'
+import AboutTeam from '../template/AboutTeam'
+
+function AboutUsPage() {
+  return (
+   <>
+   <MeetTheTeam/>
+   <AboutTeam/>
+   </>
+  )
+}
+
+export default AboutUsPage
