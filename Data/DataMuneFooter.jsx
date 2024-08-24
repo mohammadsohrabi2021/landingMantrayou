577db8283e1b23221cba6be3d5e1f3b046126bfe
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-08-24 15:41:39 +0330

    create data file pages and resources done

---
+++ Data/DataMuneFooter.jsx
@@ -0,0 +1,11 @@
+export const pages = [
+    { name: "خانه", href: "/" },
+    { name: "درباره ما", href: "/about-us" },
+    { name: "تماس با ما", href: "/contact-us" },
+  ];
+  
+  export const resources = [
+    { name: "ورود", href: "/login" },
+    { name: "شروع رایگان", href: "/get-started" },
+    { name: "پرسش‌های متداول", href: "/faq" },
+  ];
