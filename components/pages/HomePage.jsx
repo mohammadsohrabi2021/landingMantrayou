51ceb37b210dd60a9d066941c44a3cf13b62e50b
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-08-26 17:39:38 +0330

    update component HomePage done

--- components/pages/HomePage.jsx
+++ components/pages/HomePage.jsx
@@ -19,7 +19,7 @@ function HomePage() {
       <MantraYouBanner/>
       <ComparisonTable/>
       <CustomerFeedbackSlider/>
-      {/* <LogoSlider/> */}
+      <LogoSlider/>
       <FAQSection  title="هنوز سوال دارید؟"
       description="در این بخش به پرسش‌های متداول پاسخ داده‌شده است، اگر سوالی دارید که پاسخ آن را نیافتید، با ما تماس بگیرید."/>
      {/* <InteractiveCoachDisplay/> */}
