0af7421ca31a4f8d180a0974471624b9fa928fc4
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-08-26 17:43:19 +0330

    update file data component dataMuneHeader done

--- Data/DataMune.jsx
+++ Data/DataMune.jsx
@@ -1,6 +1,8 @@
 export const dataMuneHeader = [
-  { id: 1, name: "صفحه اصلی", href: "/" },
-  { id: 2, name: "بررسی ها", href: "/reviews" },
-  { id: 3, name: "تیم ما", href: "/about-us" },
-  { id: 4, name: "تماس باما", href: "/contact-us" },
+  { id: 1, name: "صرافی نوری", href: "/" },
+  { id: 2, name: " ارز", href: "/currency" },
+  { id: 3, name: " سکه", href: "/coin" },
+  { id: 4, name: " مسکوکات بانکی", href: "/bank-notes" },
+  { id: 5, name: " درباره ما", href: "/about-us" },
+  { id: 6, name: "تماس باما", href: "/contact-us" },
 ];
